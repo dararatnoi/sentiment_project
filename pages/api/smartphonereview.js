8af846e8687e93d55cb6712bdf44b081ad8b722a
dararatnoi <dararat.ta@kkumail.com>
2024-03-02 03:39:46 +0700

    Updatechart 'Customer Feelings for Smartphone'

--- pages/api/smartphonereview.js
+++ pages/api/smartphonereview.js
@@ -11,8 +11,130 @@ export default async function handler(req, res) {
     //   res.json(myPost.ops[0]);
     //   break;
     case "GET":
-      const allReview = await db.collection("SmartphoneReviewTrue").find({ is_sentiment_comment: true }).toArray();
-      res.json(allReview);
+      const count_all = await db.collection("SmartphoneReview").countDocuments({is_sentiment_comment:true});
+      const count_pos = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "pos" } });
+      const count_neu = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neu" } });
+      const count_neg = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neg" } });
+      const count_apple_pos = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "pos" },Brand:{$eq:"Apple"}});
+      const count_apple_neu = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neu" },Brand:{$eq:"Apple"}});
+      const count_apple_neg = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neg" },Brand:{$eq:"Apple"}});
+      const count_oppo_pos = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "pos" },Brand:{$eq:"Oppo"}});
+      const count_oppo_neu = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neu" },Brand:{$eq:"Oppo"}});
+      const count_oppo_neg = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neg" },Brand:{$eq:"Oppo"}});
+      const count_samsung_pos = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "pos" },Brand:{$eq:"Samsung"}});
+      const count_samsung_neu = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neu" },Brand:{$eq:"Samsung"}});
+      const count_samsung_neg = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neg" },Brand:{$eq:"Samsung"}});
+      const count_vivo_pos = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "pos" },Brand:{$eq:"vivo"}});
+      const count_vivo_neu = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neu" },Brand:{$eq:"vivo"}});
+      const count_vivo_neg = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neg" },Brand:{$eq:"vivo"}});
+      const count_xiaomi_pos = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "pos" },Brand:{$eq:"Xiaomi"}});
+      const count_xiaomi_neu = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neu" },Brand:{$eq:"Xiaomi"}});
+      const count_xiaomi_neg = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neg" },Brand:{$eq:"Xiaomi"}});
+      const count_huawei_pos = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "pos" },Brand:{$eq:"Huawei"}});
+      const count_huawei_neu = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neu" },Brand:{$eq:"Huawei"}});
+      const count_huawei_neg = await db.collection("SmartphoneReview").countDocuments({ is_sentiment_comment:true,Sentiment_Label: { $eq: "neg" },Brand:{$eq:"Huawei"}});
+      await res.json(
+        {
+          overviews:
+          {
+            count_all: count_all ?? 0,
+            count_pos: count_pos ?? 0,
+            count_neu: count_neu ?? 0,
+            count_neg: count_neg ?? 0
+          },
+          brands:
+          {
+            Apple:
+            {
+              count_pos: count_apple_pos ?? 0,
+              count_neu: count_apple_neu ?? 0,
+              count_neg: count_apple_neg ?? 0
+            },
+            Oppo:
+            {
+              count_pos: count_oppo_pos ?? 0,
+              count_neu: count_oppo_neu ?? 0,
+              count_neg: count_oppo_neg ?? 0
+            },
+            Samsung:
+            {
+              count_pos: count_samsung_pos ?? 0,
+              count_neu: count_samsung_neu ?? 0,
+              count_neg: count_samsung_neg ?? 0
+            },
+            Vivo:
+            {
+              count_pos: count_vivo_pos ?? 0,
+              count_neu: count_vivo_neu ?? 0,
+              count_neg: count_vivo_neg ?? 0
+            },
+            Xiaomi:
+            {
+              count_pos: count_xiaomi_pos ?? 0,
+              count_neu: count_xiaomi_neu ?? 0,
+              count_neg: count_xiaomi_neg ?? 0
+            },
+            Huawei:
+            {
+              count_pos: count_huawei_pos ?? 0,
+              count_neu: count_huawei_neu ?? 0,
+              count_neg: count_huawei_neg ?? 0
+            }
+          }
+
+        }
+      )
       break;
   }
 }
+
+
+// {
+//   overviews: {
+//     all:0,
+//     pos:0,
+//     neg:0,
+//     neu:0
+//   },
+//   brands: {
+//     iphone: {
+//       pos:0,
+//       neg
+//     }
+//   },
+//     Asp : {
+//       Apple: {
+//         Overview: {
+//           pos:0,
+//           neu:0,
+//           neg:0
+//         },
+//         Asp: {
+//           Camera: {
+//             pos,
+//             neu,
+//             neg
+//           }
+//           Battery: {
+//             pos,
+//             neu,
+//             neg
+//           }
+//           Price: {
+//             pos,
+//             neu,
+//             neg
+//           }
+//           Perfomance: {
+//             pos,
+//             neu,
+//             neg
+//           }
+//         },
+//         Comment: [
+//           ....
+//         ]
+        
+//       }
+//     }
+// }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,16 @@ import Navbar from "@/components/navbar";
 import React, { useEffect, useState, useRef } from 'react';
 import { Card, Skeleton } from "@nextui-org/react";
 import { Chart } from "chart.js/auto";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownSection,
+  DropdownItem,
+  Button
+} from "@nextui-org/react";
+import { Checkbox } from "@nextui-org/react";
+import { Select, SelectItem } from "@nextui-org/react";
 
 
 export default function Home() {
@@ -10,9 +20,18 @@ export default function Home() {
   const [statusData, setStatusData] = React.useState(false);
   const [isLoaded, setIsLoaded] = React.useState(false);
   const [myChart, setMyChart] = useState(null);
-  let count_pos = 0;
-  let count_neu = 0;
-  let count_neg = 0;
+  const [count_all, setCountAll] = useState(0)
+  const [count_pos, setCountPos] = useState(0)
+  const [count_neu, setCountNeu] = useState(0)
+  const [count_neg, setCountNeg] = useState(0)
+  const [selectBrand, setSelectBrand] = React.useState(new Set(["Apple","Oppo","Samsung","Vivo","Xiaomi","Huawei"]));
+  // const [brandModel, setBrand] = React.useState([{name:"All"}, "Apple", "Samsung", "Oppo", "Vivo", "Xiaomi", "Huawei"]);
+  // const [brandModel, setBrand] = React.useState(["All", "Apple", "Samsung", "Oppo", "Vivo", "Xiaomi", "Huawei"]);
+  const [brandModel, setBrand] = React.useState(["Apple","Oppo","Samsung","Vivo","Xiaomi","Huawei"]);
+
+  const [a,seta] = useState(['a','b'])
+  const [brandsData, setBrandsData] = useState({})
+
   const chartRef = useRef(null)
 
 
@@ -28,213 +47,195 @@ export default function Home() {
       });
       let data = await res.json();
       // Do something with allPosts, like setting state
-      setAllReviews(data);
+      // setAllReviews(data);
+      await setCountAll(data.overviews.count_all)
+      await setCountPos(data.overviews.count_pos)
+      await setCountNeu(data.overviews.count_neu)
+      await setCountNeg(data.overviews.count_neg)
+      await setBrandsData(data.brands)
       setStatusData(true);
-      setIsLoaded(true); // Set loading state after fetching data
+      // setIsLoaded(true); // Set loading state after fetching data
     };
 
     fetchData();
-
-    if (chartRef.current) {
-      if (chartRef.current.chart) {
-        chartRef.current.chart.destroy()
-      }
-
-      const context = chartRef.current.getContext("2d")
-      const newChart = new Chart(context, {
-        type: "bar",
-        data: {
-          labels: ['Apple', 'Samsung', 'Oppo', 'Vivo', 'Xiaomi', 'Huawei'],
-          datasets: [{
-            label: 'Positive',
-            data: [6000, 5000, 4000, 4300, 1200, 3200],
-            borderWidth: 1,
-            backgroundColor: '#70c1b3'
-          }, {
-            label: 'Neutral',
-            data: [2000, 3030, 1000, 1500, 1200, 1200],
-            borderWidth: 1,
-            backgroundColor: '#EFBF38'
-          }, {
-            label: 'Negative',
-            data: [4500, 1234, 1000, 1300, 1400, 2000],
-            borderWidth: 1,
-            backgroundColor: '#dd7373'
-          }]
-        },
-        options:{
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          },
-          plugins: {
-            title: {
-              display: true,
-              text: 'Customer Feelings for Smartphone',
-              font: {
-                size: 18,
-                family: 'Kanit',
-              },
-              position: 'top'
-            }
-          }
-        }
-      })
-
-      chartRef.current.chart = newChart
-    }
-
   }, []);
 
+  if (chartRef.current) {
+    if (chartRef.current.chart) {
+      chartRef.current.chart.destroy()
+    }
+    const context = chartRef.current.getContext("2d")
+    let a= []
+    let array_pos = []
+    let array_neu = []
+    let array_neg = []
+    selectBrand.forEach(element => {
+      a.push(element)
+      let total = brandsData[element]['count_pos'] + brandsData[element]['count_neu'] + brandsData[element]['count_neg']
+      array_pos.push(brandsData[element]['count_pos'] / total * 100)
+      array_neu.push(brandsData[element]['count_neu'] / total * 100)
+      array_neg.push(brandsData[element]['count_neg'] / total * 100)
+    });
+    const newChart = new Chart(context, {
+      type: "bar",
+      data: {
+        labels: a,
+        datasets: [{
+          label: 'Positive',
+          data: array_pos,
+          borderWidth: 1,
+          backgroundColor: '#70c1b3'
+        }, {
+          label: 'Neutral',
+          data: array_neu,
+          borderWidth: 1,
+          backgroundColor: '#EFBF38'
+        }, {
+          label: 'Negative',
+          data: array_neg,
+          borderWidth: 1,
+          backgroundColor: '#dd7373'
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        plugins: {
+          title: {
+            display: true,
+            text: 'Customer Feelings for Smartphone',
+            font: {
+              size: 18,
+              family: 'Kanit',
+            },
+            position: 'top'
+          }
+        }
+      }
+    })
+
+    chartRef.current.chart = newChart
+  }
+
+  const changeCheckBox = (select) => {
+    let result = []
+    selectBrand.forEach(element => {
+      result.push(element)
+    });
+
+    // seta(result)
+  }
 
 
-return (
-  <div className="bg-costom-pbg">
-    <Navbar />
-    <div className="container mx-auto mt-8">
-      <div className="flex flex-wrap -m-3 mb-5">
-        <div className="w-full sm:w-1/2 md:w-1/4 p-3">
-          <Skeleton isLoaded={statusData} className="  shadow-md rounded-[20px]">
-            <div className="bg-custom-blue text-white shadow-md rounded-[20px] p-4 py-6 pb-8 flex justify-between">
-              <div>
 
-                <h3 className="text-2xl font-bold text-white">{statusData ? allReviews.length : ""}</h3>
+  return (
+    <div className="bg-costom-pbg">
+      <Navbar />
+      <div className="container mx-auto mt-8">
+        <div className="flex flex-wrap -m-3 mb-5">
+          <div className="w-full sm:w-1/2 md:w-1/4 p-3">
+            <Skeleton isLoaded={statusData} className="  shadow-md rounded-[20px]">
+              <div className="bg-custom-blue text-white shadow-md rounded-[20px] p-4 py-6 pb-8 flex justify-between">
+                <div>
+                  <h3 className="text-2xl font-bold text-white">{count_all}</h3>
 
-                <h6 className="text-lg font-normal text-gray-600">Reviews</h6>
+                  <h6 className="text-lg font-normal text-gray-600">Reviews</h6>
+                </div>
+                <span className="material-icons text-3xl">mode_comment</span>
               </div>
-              <span className="material-icons text-3xl">mode_comment</span>
-            </div>
-          </Skeleton>
-        </div>
+            </Skeleton>
+          </div>
 
-        <div className="w-full sm:w-1/2 md:w-1/4 p-3">
-          <Skeleton isLoaded={statusData} className="  shadow-md rounded-[20px]">
-            <div className="bg-custom-green text-white shadow-md rounded-[20px] p-4 py-6 pb-8 flex justify-between">
-              <div>
-                <h3 className="text-2xl font-bold">
-                  {
-                    statusData ? (
-                      allReviews.map((pos => {
-                        if (pos.Sentiment_Label == 'pos') {
-                          count_pos = count_pos + 1
-                        }
-                      })),
+          <div className="w-full sm:w-1/2 md:w-1/4 p-3">
+            <Skeleton isLoaded={statusData} className="  shadow-md rounded-[20px]">
+              <div className="bg-custom-green text-white shadow-md rounded-[20px] p-4 py-6 pb-8 flex justify-between">
+                <div>
+                  <h3 className="text-2xl font-bold">
+                    {
                       count_pos
-                    ) : ""
-                  }
-                </h3>
-                <h6 className="text-lg font-normal text-gray-600">Positive Reviews</h6>
+                    }
+                  </h3>
+                  <h6 className="text-lg font-normal text-gray-600">Positive Reviews</h6>
+                </div>
+                <span className="material-icons text-3xl">sentiment_satisfied_alt</span>
               </div>
-              <span className="material-icons text-3xl">sentiment_satisfied_alt</span>
-            </div>
-          </Skeleton>
-        </div>
+            </Skeleton>
+          </div>
 
-        <div className="w-full sm:w-1/2 md:w-1/4 p-3">
-          <Skeleton isLoaded={statusData} className="  shadow-md rounded-[20px]">
-            <div className="bg-custom-yellow text-white shadow-md rounded-[20px] p-4 py-6 pb-8 flex justify-between">
-              <div>
-                <h3 className="text-2xl font-bold">
-                  {
-                    statusData ? (
-                      allReviews.map((pos => {
-                        if (pos.Sentiment_Label == 'neu') {
-                          count_neu = count_neu + 1
-                        }
-                      })),
+          <div className="w-full sm:w-1/2 md:w-1/4 p-3">
+            <Skeleton isLoaded={statusData} className="  shadow-md rounded-[20px]">
+              <div className="bg-custom-yellow text-white shadow-md rounded-[20px] p-4 py-6 pb-8 flex justify-between">
+                <div>
+                  <h3 className="text-2xl font-bold">
+                    {
+
                       count_neu
-                    ) : ""
-                  }
-                </h3>
-                <h6 className="text-lg font-normal text-gray-600">Neutral Reviews</h6>
+                    }
+                  </h3>
+                  <h6 className="text-lg font-normal text-gray-600">Neutral Reviews</h6>
+                </div>
+                <span className="material-icons text-3xl">sentiment_neutral</span>
               </div>
-              <span className="material-icons text-3xl">sentiment_neutral</span>
-            </div>
-          </Skeleton>
-        </div>
+            </Skeleton>
+          </div>
 
-        <div className="w-full sm:w-1/2 md:w-1/4 p-3">
-          <Skeleton isLoaded={statusData} className="  shadow-md rounded-[20px]">
-            <div className="bg-custom-red text-white shadow-md rounded-[20px] p-4 py-6 pb-8 flex justify-between">
-              <div>
-                <h3 className="text-2xl font-bold">
-                  {
-                    statusData ? (
-                      allReviews.map((pos => {
-                        if (pos.Sentiment_Label == 'neg') {
-                          count_neg = count_neg + 1
-                        }
-                      })),
+          <div className="w-full sm:w-1/2 md:w-1/4 p-3">
+            <Skeleton isLoaded={statusData} className="  shadow-md rounded-[20px]">
+              <div className="bg-custom-red text-white shadow-md rounded-[20px] p-4 py-6 pb-8 flex justify-between">
+                <div>
+                  <h3 className="text-2xl font-bold">
+                    {
                       count_neg
-                    ) : ""
-                  }
-                </h3>
-                <h6 className="text-lg font-normal text-gray-600">Negative Reviews</h6>
+                    }
+                  </h3>
+                  <h6 className="text-lg font-normal text-gray-600">Negative Reviews</h6>
+                </div>
+                <span className="material-icons text-3xl">sentiment_very_dissatisfied</span>
               </div>
-              <span className="material-icons text-3xl">sentiment_very_dissatisfied</span>
-            </div>
-          </Skeleton>
+            </Skeleton>
+          </div>
+
         </div>
 
-      </div>
+
+        <div className="row col-12  mb-5">
+          <div className="card border-0 hov-primary"
+            style={{
+              boxShadow: "5px 5px 5px 5px rgba(197, 197, 197, 0.2)",
+              borderRadius: "20px",
+            }}>
+            <div className="card-body">
+              <div className="d-flex justify-content-end align-items-center ">
 
 
-      <div className="row col-12  mb-5">
-        <div className="card border-0 hov-primary"
-          style={{
-            boxShadow: "5px 5px 5px 5px rgba(197, 197, 197, 0.2)",
-            borderRadius: "20px",
-          }}>
-          <div className="card-body">
-            <div className="d-flex justify-content-end align-items-center ">
+                  <Select
+                    label="Brand"
+                    selectionMode="multiple"
+                    placeholder="Select Brand"
+                    selectedKeys={selectBrand}
+                    onSelectionChange={setSelectBrand}
+                  >
+                    {brandModel.map((item) => (
+                      <SelectItem key={item} value={item} onChange={changeCheckBox(item)}>
+                       {item}
+                      </SelectItem>
+                    ))}
+                  </Select>
 
-              <div className="relative inline-block text-left">
-                <button className="bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center" type="button" id="dropdownMenuButton">
-                  Select Brands
-                </button>
-                <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
-                  <li>
-                    <input type="checkbox" id="allBrandsCheckbox" className="form-checkbox h-5 w-5 text-gray-600" value="all" defaultChecked />
-                    <label htmlFor="allBrandsCheckbox" className="ml-2 text-sm text-gray-700">All</label>
-                  </li>
-                  <li>
-                    <input type="checkbox" id="appleCheckbox" className="form-checkbox h-5 w-5 text-gray-600" value="apple" defaultChecked />
-                    <label htmlFor="appleCheckbox" className="ml-2 text-sm text-gray-700">Apple</label>
-                  </li>
-                  <li>
-                    <input type="checkbox" id="samsungCheckbox" className="form-checkbox h-5 w-5 text-gray-600" value="samsung" defaultChecked />
-                    <label htmlFor="samsungCheckbox" className="ml-2 text-sm text-gray-700">Samsung</label>
-                  </li>
-                  <li>
-                    <input type="checkbox" id="huaweiCheckbox" className="form-checkbox h-5 w-5 text-gray-600" value="huawei" defaultChecked />
-                    <label htmlFor="huaweiCheckbox" className="ml-2 text-sm text-gray-700">Huawei</label>
-                  </li>
-                  <li>
-                    <input type="checkbox" id="vivoCheckbox" className="form-checkbox h-5 w-5 text-gray-600" value="vivo" defaultChecked />
-                    <label htmlFor="vivoCheckbox" className="ml-2 text-sm text-gray-700">Vivo</label>
-                  </li>
-                  <li>
-                    <input type="checkbox" id="xiaomiCheckbox" className="form-checkbox h-5 w-5 text-gray-600" value="xiaomi" defaultChecked />
-                    <label htmlFor="xiaomiCheckbox" className="ml-2 text-sm text-gray-700">Xiaomi</label>
-                  </li>
-                  <li>
-                    <input type="checkbox" id="oppoCheckbox" className="form-checkbox h-5 w-5 text-gray-600" value="oppo" defaultChecked />
-                    <label htmlFor="oppoCheckbox" className="ml-2 text-sm text-gray-700">Oppo</label>
-                  </li>
-                </ul>
+
               </div>
-
-            </div>
-            <div>
-              <canvas ref={chartRef} width="50" height="18"></canvas>
+              <div>
+                <canvas ref={chartRef} width="50" height="18"></canvas>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
-)
+  )
 
 }
